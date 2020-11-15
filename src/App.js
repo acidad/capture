c8e2b29917dfbef893d2ae6f513d6473b3cf9ecf
Ben Chan <benched84@gmail.com>
2020-11-15 21:24:56 +0800

    Added pages and created AboutUs.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import AboutUs from './pages/AboutUs';
+
 function App() {
 	return (
 		<div className='App'>
 			<h1>Capture</h1>
+			<AboutUs />
 		</div>
 	);
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import AboutSection from '../components/AboutSection';
+
+function AboutUs() {
+	return (
+		<div className='about-us'>
+			<AboutSection />
+		</div>
+	);
+}
+
+export default AboutUs;
