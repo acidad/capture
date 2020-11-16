19e06e96e3fb7eb0427dab3209832f8a1ed2f05d
Ben Chan <benched84@gmail.com>
2020-11-16 22:38:10 +0800

    Global and Services styling

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+// font-family: 'Lobster', cursive;
+
 const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
@@ -9,6 +11,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
   }
 
   button{
@@ -20,6 +23,7 @@ const GlobalStyle = createGlobalStyle`
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+
     
     &:hover{
       background-color: #23d997;
