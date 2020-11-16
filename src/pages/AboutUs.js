894d312bf4b136b8c21085104a011f04ff06c639
Ben Chan <benched84@gmail.com>
2020-11-16 20:13:35 +0800

    faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import { Fragment } from 'react';
 
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
 	return (
 		<Fragment className='about-us'>
 			<AboutSection />
 			<ServicesSection />
+			<FaqSection />
 		</Fragment>
 	);
 };
